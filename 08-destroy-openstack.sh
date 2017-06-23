856060268fdec5000a2998c974063e7d3b7e796e
fenar <fenar@yahoo.com>
2017-06-23 17:10:40 -0500

    Code Clean-Up

--- 08-destroy-openstack.sh
+++ 08-destroy-openstack.sh
@@ -4,8 +4,6 @@
 #
 set -ex
 model=`juju list-models |awk '{print $1}'|grep calico`
-obnum=`hostname | cut -c 10- -`
-NODE="node10ob$obnum"
 if [[ ${model:0:6} == "calico" ]]; then
      echo "Model:Calico Found -> Destroy in Progress!"
      juju destroy-model "calico" -y
