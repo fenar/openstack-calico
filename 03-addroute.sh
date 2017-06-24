6fea98bd47d9ec5b59fe86a4b2387a011105fc8b
Fatih Nar <fenar@yahoo.com>
2017-06-24 10:48:31 -0500

    Create 03-addroute.sh

---
+++ 03-addroute.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -x
+export v4num=${HOSTNAME:9}
+
+addr=`juju run --unit nova-compute/0 'ip addr show br-eth0 | grep inet'`
+
+ipaddr=${addr:9:13}
+
+NEUTRON_FIXED_NET_CIDR="192.168.$((v4num)).0/24"
+
+echo "NOVA-COMPUTE Address: ${ipaddr}"
+
+sudo route add -net $NEUTRON_FIXED_NET_CIDR gw ${ipaddr}
